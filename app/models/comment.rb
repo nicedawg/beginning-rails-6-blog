1fc2a12fa441c348abfe86a36fd7203c5ebadcc5
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 20:45:50 -0600

    Add after_create callback to email author when comment is created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,13 @@ class Comment < ApplicationRecord
   validates :name, :email, :body, presence: true
   validate :article_should_be_published
 
+  after_create :email_article_author
+
   def article_should_be_published
     errors.add(:article_id, 'is not published yet') if article && !article.published?
+  end
+
+  def email_article_author
+    puts "We will notify #{article.user.email} in Chapter 12" if article.user
   end
 end
