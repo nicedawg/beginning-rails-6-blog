fcbec97f94c413bbc9f601eee539aec28e0f254a
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 18:54:06 -0600

    Add the Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_020148) do
+ActiveRecord::Schema.define(version: 2020_02_14_004535) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_02_04_020148) do
     t.string "location"
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
