ac5613a267472458e4354672cb58643aba6d1959
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 19:56:49 -0600

    Add scopes to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,11 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :comments
 
+  scope :published, -> { where.not(published_at: nil) }
+  scope :draft, -> { where(published_at: nil) }
+  scope :recent, -> { where('articles.published_at > ?', 1.week.ago.to_date) }
+  scope :where_title, -> (term) { where("articles.title LIKE ?", "%#{term}%") }
+
   def long_title
     "#{title} - #{published_at}"
   end
