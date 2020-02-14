9ff19ebd03299de0211acbc9f1b4a1f4b4c1d1c7
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 20:59:47 -0600

    Encrypt user passwords and add authentication methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_010834) do
+ActiveRecord::Schema.define(version: 2020_02_14_024558) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_010834) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "hashed_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
