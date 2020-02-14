b012b68a558f155204e60783275c28d4c51ab492
Brady Somerville <brady.somerville@gmail.com>
2020-02-14 14:20:23 -0600

    Create some articles for the user in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create email: 'mary@example.com', password: 'guessit'
+user = User.create email: 'mary@example.com', password: 'guessit', password_confirmation: 'guessit'
 
 Category.create [
   {name: 'Programming'},
@@ -7,3 +7,21 @@ Category.create [
   {name: 'Music'},
   {name: 'TV'}
 ]
+
+user.articles.create([
+  {
+    title: 'Advanced Active Record',
+    body: "Models need to relate to each other. In the real world, ..",
+    published_at: Date.today,
+  },
+  {
+    title: 'One-to-many associations',
+    body: "One-to-many associations describe a pattern ..",
+    published_at: Date.today
+  },
+  {
+    title: 'Associations',
+    body: "Active Record makes working with associations easy..",
+    published_at: Date.today
+  },
+])
