37f9c23935054fbe5793c6c4dba4b14a30629d4f
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 19:34:16 -0600

    Add Comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_004646) do
+ActiveRecord::Schema.define(version: 2020_02_14_010834) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2020_02_14_004646) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.string "name"
+    t.string "email"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name"
@@ -57,5 +67,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_004646) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
   add_foreign_key "profiles", "users"
 end
