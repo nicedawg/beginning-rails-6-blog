44508e2ef970ff0c9321d5dbb35751d2ed1a1188
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 20:19:51 -0600

    Add password confirmation validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, length: { in: 5..50 }
   validates :email, format: { with:  /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i }
+  validates :password, confirmation: true
 
   has_one :profile
   has_many :articles, -> { order 'published_at DESC, title ASC' },
