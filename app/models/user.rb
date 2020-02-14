3c5cb52ceb96afdb2d851fa8791da5e066f88df0
Brady Somerville <brady.somerville@gmail.com>
2020-02-13 20:15:00 -0600

    Validate uniqueness, length, and format of user#email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :email, length: { in: 5..50 }
+  validates :email, format: { with:  /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i }
+
   has_one :profile
   has_many :articles, -> { order 'published_at DESC, title ASC' },
            dependent: :nullify
