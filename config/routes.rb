fe335a5aa130dd296634ddeb19f1548c7ba0c03a
Brady Somerville <brady.somerville@gmail.com>
2020-02-24 20:03:58 -0600

    Add ability to login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  resource :session
+  get "/login", to: "sessions#new", as: "login"
+  get "/logout", to: "sessions#destroy", as: "logout"
 end
