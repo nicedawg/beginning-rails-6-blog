0f8b6d42bb5503cc67e6db73679aced5fd369b6a
Brady Somerville <brady.somerville@gmail.com>
2020-02-28 20:27:00 -0600

    Submit comment form via ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,15 @@ class CommentsController < ApplicationController
   def create
     @comment = @article.comments.new(comment_params)
     if @comment.save
-      redirect_to @article, notice: 'Thanks for your comment'
+      respond_to do |format|
+        format.html { redirect_to @article, notice: 'Thanks for your comment' }
+        format.js
+      end
     else
-      redirect_to @article, alert: 'Unable to add comment'
+      respond_to do |format|
+        format.html { redirect_to @article, notice: 'Unable to add comment' }
+        format.js { render :fail_create }
+      end
     end
   end
 
