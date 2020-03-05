0e596be9ba83c092388736793c861ef4b1d649d2
Brady Somerville <brady.somerville@gmail.com>
2020-03-04 21:20:58 -0600

    Configure Article#body to be a RichText relation

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,6 +9,8 @@ class Article < ApplicationRecord
   attr_accessor :remove_cover_image
   after_save { cover_image.purge if remove_cover_image == '1' }
 
+  has_rich_text :body
+
   scope :published, -> { where.not(published_at: nil) }
   scope :draft, -> { where(published_at: nil) }
   scope :recent, -> { where('articles.published_at > ?', 1.week.ago.to_date) }
