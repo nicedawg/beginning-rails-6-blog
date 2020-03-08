334f7f3060a2000d20883b814f2b3d016244bdef
Brady Somerville <brady.somerville@gmail.com>
2020-03-08 16:10:53 -0500

    Configure SMTP settings for ActionMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+
+  # Gmail SMTP server setup
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    enable_starttls_auto: true,
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.credentials.smtp[:user_name],
+    password: Rails.application.credentials.smtp[:password],
+  }
 
   config.action_mailer.perform_caching = false
 
