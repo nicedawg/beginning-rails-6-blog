a73ab57cdee50a4b449609af1afe1c5f446f7180
Brady Somerville <brady.somerville@gmail.com>
2020-03-12 21:21:00 -0500

    Update mailer preview for NotifierMail.email_friend

--- test/mailers/previews/notifier_mailer_preview.rb
+++ test/mailers/previews/notifier_mailer_preview.rb
@@ -3,7 +3,7 @@ class NotifierMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/notifier_mailer/email_friend
   def email_friend
-    NotifierMailer.email_friend
+    NotifierMailer.email_friend(Article.first, 'Sender T. Sendington', 'ree.seever@example.com')
   end
 
 end
