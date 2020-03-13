f352185de5d9eeb39ba36213e5addfc15f5a5797
Brady Somerville <brady.somerville@gmail.com>
2020-03-12 20:50:32 -0500

    Pass data to NotifierMailer#email_friend

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,13 +1,8 @@
 class NotifierMailer < ApplicationMailer
+  def email_friend(article, sender_name, receiver_email)
+    @article = article
+    @sender_name = sender_name
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notifier_mailer.email_friend.subject
-  #
-  def email_friend
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    mail to: receiver_email, subject: 'Interesting Article'
   end
 end
