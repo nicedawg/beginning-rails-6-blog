1e2d5c2e585b15959aabb1294ae959d0339177dd
Brady Somerville <brady.somerville@gmail.com>
2020-03-28 14:47:02 -0500

    Retry GuessANumberBetweenOneAndTenJob when it fails to guess our number

--- app/jobs/guess_a_number_between_one_and_ten_job.rb
+++ app/jobs/guess_a_number_between_one_and_ten_job.rb
@@ -1,13 +1,17 @@
 class GuessANumberBetweenOneAndTenJob < ApplicationJob
   queue_as :default
 
+  class GuessedWrongNumber < StandardError; end
+
+  retry_on GuessedWrongNumber, attempts: 8, wait: 1
+
   def perform(my_number)
     guessed_number = rand(1..10)
 
     if guessed_number == my_number
       Rails.logger.info "I guessed it! It was #{my_number}"
     else
-      Rails.logger.error "Is it #{guessed_number}? No? Hmm."
+      raise GuessedWrongNumber, "Is it #{guessed_number}? No? Hmm."
     end
   end
 end
