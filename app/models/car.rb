77b5b4be405b81380a2f41a7fe1f36ad557d5a8b
Brady Somerville <brady.somerville@gmail.com>
2020-03-29 14:28:09 -0500

    Allow assigning of attributes in Car.new

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,13 @@
 class Car
+  include ActiveModel::AttributeAssignment
+
   attr_accessor :make, :model, :year, :color
+
+  def initialize(attributes = {})
+    assign_attributes(attributes) if attributes
+
+    super()
+  end
 
   def paint(new_color)
     self.color = new_color
