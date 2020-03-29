12540d2483e5afd13f139ea66a7687d052fb9ce2
Brady Somerville <brady.somerville@gmail.com>
2020-03-29 16:05:13 -0500

    Use ActiveModel::Dirty to only notify DMV if car being painted a different color than before

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,13 +1,15 @@
 class Car
   include ActiveModel::AttributeAssignment
   extend ActiveModel::Callbacks
+  include ActiveModel::Dirty
 
   attr_accessor :make, :model, :year, :color
 
+  define_attribute_methods :color
   define_model_callbacks :paint
 
   before_paint :keep_it_waxed
-  after_paint :notify_dmv
+  after_paint :notify_dmv, if: :color_changed?
 
   def initialize(attributes = {})
     assign_attributes(attributes) if attributes
@@ -18,6 +20,8 @@ class Car
   def paint(new_color)
     run_callbacks :paint do
       Rails.logger.info "Painting the car #{new_color}"
+
+      color_will_change! if color != new_color
       self.color = new_color
     end
   end
@@ -30,5 +34,6 @@ class Car
 
   def notify_dmv
     Rails.logger.warn "Be sure to notify the DMV about this color change!"
+    changes_applied
   end
 end
