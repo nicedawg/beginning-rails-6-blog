d80aa3c129754c2bd9841e5d79287574b2ff3d10
Brady Somerville <brady.somerville@gmail.com>
2020-03-29 20:04:07 -0500

    Validate Email A Friend submissions and show errors when invalid

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
+    @email_a_friend = EmailAFriend.new
   end
 
   # GET /articles/new
@@ -66,8 +67,14 @@ class ArticlesController < ApplicationController
   end
 
   def notify_friend
-    NotifierMailer.email_friend(@article, params[:name], params[:email]).deliver_later
-    redirect_to @article, notice: 'Successfully sent a message to your friend'
+    @email_a_friend = EmailAFriend.new(email_a_friend_params)
+
+    if @email_a_friend.valid?
+      NotifierMailer.email_friend(@article, @email_a_friend.name, @email_a_friend.email).deliver_later
+      redirect_to @article, notice: 'Successfully sent a message to your friend'
+    else
+      render :notify_friend, status: :unprocessable_entity
+    end
   end
 
   private
@@ -79,5 +86,9 @@ class ArticlesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :cover_image, :remove_cover_image, :location, :excerpt, :body, :published_at, category_ids: [])
+    end
+
+    def email_a_friend_params
+      params.require(:email_a_friend).permit(:name, :email)
     end
 end
