bad1e502158ca0e1f9ff7ba920dc7fa6a38bf551
Brady Somerville <brady.somerville@gmail.com>
2020-04-18 14:10:53 -0500

    Add model test for destroying an article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -21,4 +21,10 @@ class ArticleTest < ActiveSupport::TestCase
     article.update(title: 'New title')
     assert_equal 'New title', article.reload.title
   end
+
+  test 'should destroy article' do
+    article = articles(:welcome_to_rails)
+    article.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Article.find(article.id) }
+  end
 end
