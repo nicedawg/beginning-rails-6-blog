e37ab0e28d8785983ea3810f92f0a4473e087425
Brady Somerville <brady.somerville@gmail.com>
2020-04-18 14:00:26 -0500

    Add model test for finding an article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -10,4 +10,9 @@ class ArticleTest < ActiveSupport::TestCase
 
     assert article.save
   end
+
+  test 'should find article' do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id) }
+  end
 end
