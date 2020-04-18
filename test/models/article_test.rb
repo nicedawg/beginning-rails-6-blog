015d3f35e8a3cad1a03b0ec0426872653ea77584
Brady Somerville <brady.somerville@gmail.com>
2020-04-18 12:54:14 -0500

    Add model test for creating an article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create article' do
+    article = Article.new
+
+    article.user = users(:eugene)
+    article.title = 'Test Article'
+    article.body = 'Test body'
+
+    assert article.save
+  end
 end
