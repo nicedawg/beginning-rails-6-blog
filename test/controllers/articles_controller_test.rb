b2999291b49de6dfb7fcb2a1d4a1ed07300335d1
Brady Somerville <brady.somerville@gmail.com>
2020-04-18 17:01:24 -0500

    Fix ArticlesControllerTest by logging in when necessary

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -11,11 +11,13 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    login_as :eugene
     get new_article_url
     assert_response :success
   end
 
   test "should create article" do
+    login_as :eugene
     assert_difference('Article.count') do
       post articles_url, params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
     end
@@ -29,16 +31,19 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    login_as :eugene
     get edit_article_url(@article)
     assert_response :success
   end
 
   test "should update article" do
+    login_as :eugene
     patch article_url(@article), params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
     assert_redirected_to article_url(@article)
   end
 
   test "should destroy article" do
+    login_as :eugene
     assert_difference('Article.count', -1) do
       delete article_url(@article)
     end
