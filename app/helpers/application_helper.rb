5fa942570988a096ae51eeba26cd9e1c6b15343a
Brady Somerville <brady.somerville@gmail.com>
2020-04-19 17:48:24 -0500

    Replace hardcoded strings in view templates with I18n.t calls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def submit_or_cancel(form, name = "Cancel")
-    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', class: 'cancel')
+  def submit_or_cancel(form, name = t("general.cancel"))
+    form.submit + " #{t("general.or")} " + link_to(name, 'javascript:history.go(-1);', class: 'cancel')
   end
 end
