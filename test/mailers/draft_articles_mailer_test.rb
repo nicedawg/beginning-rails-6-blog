ccd4257004f1c415205f1d2fd1b2dda729070e6d
Brady Somerville <brady.somerville@gmail.com>
2020-04-18 19:52:48 -0500

    Fix DraftArticlesMailerTest

--- test/mailers/draft_articles_mailer_test.rb
+++ test/mailers/draft_articles_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class DraftArticlesMailerTest < ActionMailer::TestCase
   test "no_author" do
-    mail = DraftArticlesMailer.no_author
-    assert_equal "No author", mail.subject
+    mail = DraftArticlesMailer.no_author('to@example.org')
+    assert_equal "Your email could not be processed", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Please check your draft articles email address and try again.", mail.body.encoded
   end
 
 end
